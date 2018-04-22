ca183e79552d821ff50b5d15cb6aa74379e1e469
LAPTOP-3R3M1SHA\gsctc <gabecoach@gmail.com>
2018-04-22 14:08:00 -0500

    Separation of Concerns Modules

---
+++ MGCTrainingPortalAPI.CourseBuilder/FullTrainingCourseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MGCTrainingPortalAPI.Models;
+
+namespace MGCTrainingPortalAPI.CourseBuilder
+{
+    class FullTrainingCourse
+    {
+        public TrainingCourse training_course_information { get; set; }
+        public List<TrainingCourseModule> training_course_modules { get; set; }
+        public List<TrainingCourseModuleQuiz> training_course_module_quizs { get; set; }
+        public List<TrainingCourseModuleSection> training_course_module_sub_sections { get; set; }
+        public List<TrainingCourseModuleSection> training_course_module_sections { get; set; }
+        public List<TrainingCourseQuizScore> training_course_quiz_scores { get; set; }
+        public List<QuizQuestion> quiz_questions { get; set; }
+        public List<QuizQuestionAnswerOption> quiz_answer_options { get; set; }
+        public List<QuizQuestionCorrectAnswer> quiz_question_correct_answers { get; set; }
+    }
+}
---
+++ MGCTrainingPortalAPI/Controllers/CourseBuilderController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MGCTrainingPortalAPI.CourseBuilder;
+
+namespace MGCTrainingPortalAPI.Controllers
+{
+    public class CourseBuilderController : ApiController
+    {
+         
+    }
+}
