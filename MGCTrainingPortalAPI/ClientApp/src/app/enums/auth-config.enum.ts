0200224ba096d60fb646d676fbe9c19f5c5ebec3
Gabriel Coach <gsctca@gmail.com>
2018-05-29 17:43:05 -0500

    minor changes

--- MGCTrainingPortalAPI/ClientApp/src/app/enums/auth-config.enum.ts
+++ MGCTrainingPortalAPI/ClientApp/src/app/enums/auth-config.enum.ts
@@ -2,5 +2,5 @@ export enum AuthConfig {
     ISSUER = 'https://dev-380362.oktapreview.com/oauth2/default',
     DEV_REDIRECT_URI = 'http://localhost:4200/implicit/callback',
     CLIENT_ID = '0oaehwxf90v8H5BaX0h7',
-    PROD_REDIRECT_URI = 'http://www.mgctrainingportal.com/implicit/callback',
+    PROD_REDIRECT_URI = 'https://www.mgctrainingportal.com/implicit/callback',
 }
