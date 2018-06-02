403cc46b5daa51d5a645fa519cbf416ed630371f
Gabriel Coach <gsctca@gmail.com>
2018-06-02 10:23:01 -0500

    Deleted Quizzes Module, preparing for training courses recreation

--- MGCTrainingPortalAPI/ClientApp/src/app/components/portal/training-courses/training-courses.module.ts
+++ MGCTrainingPortalAPI/ClientApp/src/app/components/portal/training-courses/training-courses.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuizsModule } from './quizs/quizs.module';
-
 
 import { TrainingCoursesRoutingModule } from './training-courses-routing.module';
 import { ModulesComponent } from './modules/modules.component';
 import { TrainingCoursesComponent } from './training-courses.component';
 import { CoursesComponent } from './courses/courses.component';
 
-
 // Services
 import { CourseBuilderService } from './services/course-builder/course-builder.service';
 import { TrainingCourseService } from './services/training-course/training-course.service';
 import { TrainingCourseModuleService } from './services/training-course-module/training-course-module.service';
-
-
-
 
 @NgModule({
   imports: [
@@ -30,8 +24,7 @@ import { TrainingCourseModuleService } from './services/training-course-module/t
   providers: [
     CourseBuilderService,
     TrainingCourseService,
-    TrainingCourseModuleService,
-
+    TrainingCourseModuleService
   ]
 
 })
